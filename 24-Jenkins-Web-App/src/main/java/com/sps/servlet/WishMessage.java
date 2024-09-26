ef3642e8b6a2b6b8dd90ce0ae1eaaaa1df3e1eef
Samar <samarpratap9708@gmail.com>
2024-09-26 06:42:03 +0530

    Initial Commit

---
+++ 24-Jenkins-Web-App/src/main/java/com/sps/servlet/WishMessage.java
@@ -0,0 +1,31 @@
+package com.sps.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/greet")
+public class WishMessage extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		PrintWriter pw = res.getWriter();
+		res.setContentType("text/html");
+		
+		pw.print("<center><h1 style='color : red'>Hello, Good Morning</h1>");
+		pw.print("<br><br><a href='index.jsp'>Home</a></center>");
+		
+		pw.close();
+	}
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doGet(req, res);
+	}
+}
---
+++ 24-Jenkins-Web-App/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<center>
+	<h1 style='text-align:center'>Jenkins-Test Web Application</h1>
+	<br>
+	<h3 style='text-align:center'><a href='greet'>Get greet message</a></h3>
+</center>
